9957011d119f151c7df72c342b0c29a27283c50c
Shutov Nikolay <shutovnv@gmail.com>
2021-02-03 23:38:14 +0300

    task 17, chapter 4 ready

--- Chapter_4/task_17.py
+++ Chapter_4/task_17.py
@@ -4,3 +4,19 @@
 делить возраст человека (число полных лет). В случае совпадения
 указанных номеров месяцев считать, что прошел полный год.
 '''
+import datetime
+
+year = int(input("Введите год рождения человека: "))
+month = int(input("Введите месяц рождения человека: "))
+
+now = datetime.datetime.today()
+
+delta_year = now.year - year
+
+delta_month = now.month - month
+if delta_month < 0:
+    delta_year = delta_year - 1
+
+
+print ("Ответ:")
+print ("Возраст человека - "+str(delta_year)+str(" полных лет.")) 
